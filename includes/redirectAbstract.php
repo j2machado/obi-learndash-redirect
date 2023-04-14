ec9812191b42f90189f69ba986030100b3380d1f
Obi Juan <hola@obijuan.dev>
2023-04-14 14:08:17 -0500

    Add redirectAbstract.php file

---
+++ includes/redirectAbstract.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Obi_LearnDash_Redirect;
+
+abstract class redirectAbstract{
+
+    protected static $courseOptionsSerialized;
+
+    protected static $courseAccessMode;
+
+    protected static $courseButtonURL;
+
+
+    public function __construct(){
+
+    }
+
+    protected static function getOptionsSerialized(){
+
+    }
+
+    protected static function getAccessMode(){
+
+    }
+
+    protected static function getButtonURLvalue(){
+
+    }
+
+    protected static function redirectTheEndUser(){
+
+        
+
+    }
+
+
+}
